7795aa4e6a28b73b5e8a6acedf0827807af185ab
khanhlt <ltk.hust@gmail.com>
2017-12-05 19:43:23 +0700

    config base layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,87 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <style>
+        .mySlides {
+            display: none
+        }
+
+        .w3-left, .w3-right, .w3-badge {
+            cursor: pointer
+        }
+
+        .w3-badge {
+            height: 13px;
+            width: 13px;
+            padding: 0
+        }
+    </style>
+    <div class="w3-container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
-
                     <div class="panel-body">
                         @if (session('status'))
                             <div class="alert alert-success">
                                 {{ session('status') }}
                             </div>
                         @endif
+                        <div class="w3-container">
+                            <h2>Let make your every is a small progress!</h2>
+                            <p>Better health with various fitness courses</p>
+                        </div>
 
-                        You are logged in!
+
+                        <div class="w3-content" style="max-width:800px">
+                            <img class="mySlides" src="https://www.w3schools.com/w3css/img_nature_wide.jpg" style="width:100%">
+                            <img class="mySlides" src="https://www.w3schools.com/w3css/img_fjords_wide.jpg" style="width:100%">
+                            <img class="mySlides" src="https://www.w3schools.com/w3css/img_mountains_wide.jpg" style="width:100%">
+                        </div>
+
+                        <div class="w3-center">
+                            <div class="w3-section">
+                                <button class="w3-button w3-light-grey" onclick="plusDivs(-1)">❮ Prev</button>
+                                <button class="w3-button w3-light-grey" onclick="plusDivs(1)">Next ❯</button>
+                            </div>
+                            <button class="w3-button demo" onclick="currentDiv(1)">1</button>
+                            <button class="w3-button demo" onclick="currentDiv(2)">2</button>
+                            <button class="w3-button demo" onclick="currentDiv(3)">3</button>
+                        </div>
+
+                        <script>
+                            var slideIndex = 1;
+                            showDivs(slideIndex);
+
+                            function plusDivs(n) {
+                                showDivs(slideIndex += n);
+                            }
+
+                            function currentDiv(n) {
+                                showDivs(slideIndex = n);
+                            }
+
+                            function showDivs(n) {
+                                var i;
+                                var x = document.getElementsByClassName("mySlides");
+                                var dots = document.getElementsByClassName("demo");
+                                if (n > x.length) {
+                                    slideIndex = 1
+                                }
+                                if (n < 1) {
+                                    slideIndex = x.length
+                                }
+                                for (i = 0; i < x.length; i++) {
+                                    x[i].style.display = "none";
+                                }
+                                for (i = 0; i < dots.length; i++) {
+                                    dots[i].className = dots[i].className.replace(" w3-red", "");
+                                }
+                                x[slideIndex - 1].style.display = "block";
+                                dots[slideIndex - 1].className += " w3-red";
+                            }
+                        </script>
                     </div>
                 </div>
             </div>
