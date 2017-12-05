8c3b2351912be73861b5936432eb9d223e8b6657
khanhlt <ltk.hust@gmail.com>
2017-12-05 15:07:36 +0700

    [blog]: use tinyMCE lib for edit post + upload image

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -10,6 +10,14 @@
     Add New Post
 @endsection
 @section('content')
+    <script type="text/javascript" src="{{ asset('/js/tinymce/tinymce.min.js') }}"></script>
+    <script type="text/javascript">
+        tinymce.init({
+            selector : "textarea",
+            plugins : ["advlist autolink lists link image charmap print preview anchor", "searchreplace visualblocks code fullscreen", "insertdatetime media table contextmenu paste jbimages"],
+            toolbar : "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image jbimages",
+        });
+    </script>
     <div class="container">
         <div class="row">
             <form action="/new-post" method="post">
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -9,6 +9,14 @@
     Edit Post
 @endsection
 @section('content')
+    <script type="text/javascript" src="{{ asset('/js/tinymce/tinymce.min.js') }}"></script>
+    <script type="text/javascript">
+        tinymce.init({
+            selector : "textarea",
+            plugins : ["advlist autolink lists link image charmap print preview anchor", "searchreplace visualblocks code fullscreen", "insertdatetime media table contextmenu paste jbimages"],
+            toolbar : "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image jbimages",
+        });
+    </script>
     <div class="container">
         <div class="row">
             <form method="post" action='{{ url("/update") }}'>
