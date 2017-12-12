aa370193aab4ae1f9f7f6fd241a97061aa0736d6
khanhlt <ltk.hust@gmail.com>
2017-12-12 22:59:11 +0700

    edit layout

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -1,3 +1,8 @@
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 @extends('layouts.app')
 
 @section('content')
@@ -5,12 +10,15 @@
         <form action="{{route('reg_course')}}" method="post">
             {{csrf_field()}}
             <fieldset>
-                <input type="submit" name="Submit">
-                <table>
+                <input class="btn btn-primary" type="submit" name="submit" value="Registration">
+                <table class="table table-hover">
+                    <thead>
                     <tr>
                         <th>Course name</th>
                         <th>Done</th>
                     </tr>
+                    </thead>
+                    <tbody>
                     @foreach($course as $course_item)
                         <tr>
                             <td><h1>{{$course_item->title}}</h1></td>
@@ -19,9 +27,10 @@
                         </tr>
 
                     @endforeach
+                    </tbody>
                 </table>
 
-
+                {!! $course->links() !!}
             </fieldset>
         </form>
     </div>
