30cd2c125e624ab91a0f1a54c6092556dbfd58e4
khanhlt <ltk.hust@gmail.com>
2017-12-12 23:10:43 +0700

    edit layout

--- resources/views/posts/myposts.blade.php
+++ resources/views/posts/myposts.blade.php
@@ -10,28 +10,27 @@
                 <div class="">
                     @foreach( $posts as $post )
                         {{--<div class="list-group" style="width: 90%">--}}
-                        <div class="list-group-item" style="height: auto">
-                            <div class="row">
-                                <div class="col-sm-10">
-                                    <h3><a href="{{ url('/'.$post->slug) }}"
-                                           style="font-size: 25px; font-family: 'Courier New', Courier, monospace">{{ $post->title }}</a>
-                                    </h3><br>
-                                    {{ $post->created_at->format('M d,Y \a\t h:i a') }} By
-                                    <a href="{{ url('/user/'.$post->author_id)}}">{{ $post->author->name }}</a>
-                                </div>
+                        <div class="list-group-item" style="height: auto; border: hidden">
+                            <div class="col-sm-10">
+                                <h3><a href="{{ url('/'.$post->slug) }}"
+                                       style="font-size: 25px; font-family: 'Courier New', Courier, monospace">{{ $post->title }}</a>
+                                </h3><br>
+                                {{ $post->created_at->format('M d,Y \a\t h:i a') }} By
+                                <a href="{{ url('/user/'.$post->author_id)}}">{{ $post->author->name }}</a>
+                            </div>
 
-                                <div class="col-sm-2">
-                                    @if($post->active == '1')
-                                        <a href="{{ url('edit/'.$post->slug)}}">Edit Post</a>
-                                    @else
-                                        <a href="{{ url('edit/'.$post->slug)}}">Edit Draft</a>
-                                    @endif
-                                </div>
+                            <div class="col-sm-2">
+                                @if($post->active == '1')
+                                    <a href="{{ url('edit/'.$post->slug)}}">Edit Post</a>
+                                @else
+                                    <a href="{{ url('edit/'.$post->slug)}}">Edit Draft</a>
+                                @endif
                             </div>
                             <p><br></p>
                         </div>
 
-                        <div class="list-group-item" style="height: auto">
+                        <div class="list-group-item"
+                             style="height: auto; border-left: hidden; border-right: hidden; border-bottom: hidden">
                             <article>
                                 {!! str_limit($post->body, $limit = 1500, $end = '....... <a href='.url("/".$post->slug).'>Read More</a>') !!}
                             </article>
